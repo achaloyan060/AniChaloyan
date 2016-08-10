4fd79d014711539dbadfc285abb190b9bbda6479
achaloyan060 <achaloyan060@users.noreply.github.com>
2016-08-10 09:40:09 -0700

    Add files via upload

---
+++ Portfolio/myScript.js
@@ -0,0 +1,17 @@
+function myFunction()
+{
+	window.alert("Welcome to Ani's website!");
+	var image_list = ["http://i.imgur.com/QaRrsqe.png", "http://i.imgur.com/1jxtWgp.png", "http://i.imgur.com/xdBvSfe.png"];
+	var item = 0;
+}
+
+function bigImg(x) {
+    x.style.height = "341px";
+    x.style.width = "517px";
+}
+
+function normalImg(x) {
+    x.style.height = "241px";
+    x.style.width = "417px";
+}
+
